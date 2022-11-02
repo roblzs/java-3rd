23641f2bc734828e9d2b1b8f6516a5d7fcd86366
RobzLegz <robzlegz@gmail.com>
2022-11-02 14:53:36 +0200

    xd

---
+++ THIRD.md
@@ -0,0 +1,14 @@
+```java
+int[] arr = new int[5];
+
+arr = [5, 3, 6, 7, 2]
+//                 ^ goes to left
+//              ^ goes to right
+//           ^ goest to right
+//        ^ goes to left      
+//     ^ checker
+
+```
+
+To -> means i + 1
+to <- means i - 1
--- third.java
+++ third.java
@@ -1,5 +1,69 @@
+import java.util.Random;
+
 public class third {
-    public static void main(String[] args){
-        
+
+  public static int rand(int min, int max) {
+    Random random = new Random();
+
+    return random.nextInt(max - min) + min;
+  }
+
+  public static int[] gen_arr() {
+    int[] arr = new int[5];
+
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = rand(0, 100);
     }
+
+    return arr;
+  }
+
+  public static void print_arr(int[] arr) {
+    String prnt = "";
+
+    for (int i = 0; i < arr.length; i++) {
+      prnt += arr[i];
+      prnt += ", ";
+    }
+
+    System.out.println(prnt);
+  }
+
+  public static int partiotion(int[] arr, int low, int high) {
+    int i = low - 1;
+
+    for (int j = low; j <= high; j++) {}
+
+    return 0;
+  }
+
+  public static void quick_sort(int[] arr, int x) {
+    while (x < arr.length) {
+      int xel = arr[x];
+
+      for (int i = 0; i < arr.length; i++) {
+        int el = arr[i];
+
+        if (el < xel) {
+          arr[x - 1] = el;
+        } else if (el > xel) {
+          arr[x + 1] = el;
+        }
+      }
+
+      x++;
+    }
+
+    System.out.println("Sorted array: ");
+    print_arr(arr);
+  }
+
+  public static void main(String[] args) {
+    int[] arr = gen_arr();
+
+    System.out.println("Generated array: ");
+    print_arr(arr);
+
+    quick_sort(arr, 0);
+  }
 }
